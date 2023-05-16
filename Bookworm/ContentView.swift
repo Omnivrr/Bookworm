915330538a1396712d3f8afe38c659a52762e112
Omnivrr <Omnivrr@gmail.com>
2023-05-16 12:42:10 +0300

    CoreData

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
     @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
     
     
@@ -16,7 +17,20 @@ struct ContentView: View {
             List(students) { student in
                 Text(student.name ?? "Unknown")
             }
-            
+            Button("Add") {
+                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+                
+                let chosenFirstName = firstNames.randomElement()!
+                let chosenLastName = lastNames.randomElement()!
+                
+                let student = Student(context: moc)
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)"
+                
+                try? moc.save()
+                
+            }
         }
     }
 }
